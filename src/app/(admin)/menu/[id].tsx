538f8ba2dd7ffa69578c9ffd228b8e609cef4f73
unknown <Joshuaolugotun17@gmail.com>
2024-07-31 02:15:20 +0530

    create screen

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -7,7 +7,9 @@ import { useState } from "react";
 import Button from "@/components/Button";
 import { ScrollView } from "react-native";
 import { useCart } from "@/providers/CartProvider";
-import { useRouter } from "expo-router";
+import { useRouter, Link } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 const ProductDetailScreen = () => {
@@ -26,7 +28,29 @@ const router = useRouter();
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Stack.Screen options={{ title: product?.name }} />
+        <Stack>
+
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+            <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="plus-square-o"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+        />
+        </Stack>
         <Image
           source={{ uri: product?.image ?? defaultPizzaImage }}
           style={styles.image}
